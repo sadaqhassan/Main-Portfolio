09ce1a2484fa289a18158ced56492597e0f8419f
sadaq <sadaqsamor99@gmail.com>
2025-09-13 22:49:20 +0300

    Routes import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Nav from './Components/Nav'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
    <>
    <Nav/>
+   <>
+   <Routes>
+    <Route></Route>
+   </Routes>
+   </>
    </>
   )
 }
